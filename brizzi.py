326c47da8ff0c02206b18b8065c1ad5f28a26057
Eka Puji Widiyanto <ekapujiw2002@gmail.com>
2019-07-15 23:33:17 +0700

    Update brizzi.py

--- brizzi.py
+++ brizzi.py
@@ -85,74 +85,14 @@ class ACR_Brizzi:
                 self._logger and self._logger.debug("Transmit to PICC = {}".format(apdu_text))
                 data, sw1, sw2 = self._reader_picc_connection.transmit(toBytes(apdu_text))
                 
-            self._logger and self._logger.debug("Reply = {}\r\nSW1 SW2{}".format(toHexString(data), toHexString(sw1), toHexString(sw2)))
+            self._logger and self._logger.debug("Reply = %s\r\nSW1 SW2 = %02X%02X".format(toHexString(data), sw1, sw2))
         except Exception as err:
             pass
             self._logger and self._logger.error(err)
             data = sw1 = sw2 = None
             
         return data, sw1, sw2
-
-'''
-# define the APDUs used in this script
-SELECT = toBytes("00A4040C09A00000000000000011")
-SELECT_AID1 = toBytes("5A010000")
-GET_CARD_NUMBER = toBytes("BD00000000170000") 
-GET_CARD_STATUS = toBytes("BD01000000200000") 
-SELECT_AID3 = toBytes("5A030000") 
-REQUEST_KEY_CARD = toBytes("0A00")
-GET_CARD_UID = toBytes("FFCA000000" )
-
-# get all the available readers
-r = readers()
-print("Available readers:", r) 
-
-# samcard
-sam_reader = r[1]
-print("Using:", sam_reader)
-
-sam_connection = sam_reader.createConnection()
-sam_connection.connect()
-
-data, sw1, sw2 = sam_connection.transmit(SELECT)
-print(toHexString(data))
-print("Select Applet: %02X %02X" % (sw1, sw2))
-
-# picc
-picc_reader = r[0]
-print("Using:", picc_reader)
-
-picc_connection = picc_reader.createConnection()
-picc_connection.connect()
-
-data, sw1, sw2 = picc_connection.transmit(SELECT_AID1)
-print(toHexString(data))
-print("Select Applet: %02X %02X" % (sw1, sw2))
-
-data, sw1, sw2 = picc_connection.transmit(GET_CARD_NUMBER)
-print(toHexString(data))
-print("Select Applet: %02X %02X" % (sw1, sw2))
-
-data, sw1, sw2 = picc_connection.transmit(GET_CARD_STATUS)
-print(toHexString(data))
-print("Select Applet: %02X %02X" % (sw1, sw2))
-
-data, sw1, sw2 = picc_connection.transmit(SELECT_AID3)
-print(toHexString(data))
-print("Select Applet: %02X %02X" % (sw1, sw2))
-
-data, sw1, sw2 = picc_connection.transmit(REQUEST_KEY_CARD)
-print(toHexString(data))
-print("Select Applet: %02X %02X" % (sw1, sw2))
-
-data, sw1, sw2 = picc_connection.transmit(GET_CARD_UID)
-print(toHexString(data))
-print("Select Applet: %02X %02X" % (sw1, sw2))
-
-sam_connection.disconnect()
-picc_connection.disconnect()
-'''
-
+		
 readerx = ACR_Brizzi(LOGGER_MAIN)
 readerx.sendAPDU(readerx.SAMCARD_SELECT)
 readerx.closeConnection()
